e0844c36c1c7d160c7c83132fd6a6086d82f019e
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-04-16 19:56:14 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Header } from "./components/Header/Header";
+import { Hero } from "./components/Hero/Hero";
+import { MainSection } from "./components/MainSection/MainSection";
+
 function App() {
   return (
-    <div>
-    </div>
+    <>
+    <Header/>
+    <Hero/>
+    <MainSection/>
+    </>
   );
 }
 
