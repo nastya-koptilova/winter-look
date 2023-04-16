e60d32574c8e0816957225ac6d7fbc5a96fd10cb
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-04-16 19:09:45 +0300

    add elements and header

---
+++ src/components/CreateImage/CreateImage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CreateImage = () => {
+  return (
+    <div>CreateImage</div>
+  )
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header>
+      <p>landing</p>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Clothes</a>
+          </li>
+          <li>
+            <a href="#">Sneakers</a>
+          </li>
+          <li>
+            <a href="#">Bags</a>
+          </li>
+          <li>
+            <a href="#">Accessorize</a>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <p>buy</p>
+      </div>
+    </header>
+  );
+};
