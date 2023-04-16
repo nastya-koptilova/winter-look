251da6e00464e3071df352d887564198823df416
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-04-16 20:05:00 +0300

    ass wrapper

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { Header } from "./components/Header/Header";
 import { Hero } from "./components/Hero/Hero";
 import { MainSection } from "./components/MainSection/MainSection";
+import s from './App.module.css';
 
 function App() {
   return (
-    <>
+    <div className={s.wrapper}>
     <Header/>
     <Hero/>
     <MainSection/>
-    </>
+    </div>
   );
 }
 
