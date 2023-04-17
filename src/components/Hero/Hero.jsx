a93ff7d7db24eceee35d411baa33755b37c9f648
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-04-18 00:22:06 +0300

    add hero styles

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { CreateImage } from "../CreateImage/CreateImage";
 import { AppAviable } from "../AppAviable/AppAviable";
+import s from "./Hero.module.scss";
 
 export const Hero = () => {
   return (
-    <section>
-      <AppAviable/>
-      <h1>
-        CHOOSE YOUR WINTER <span>LOOK</span> APPAREL
+    <section className={s.hero}>
+      <AppAviable />
+      <h1 className={s.hero__title}>
+        CHOOSE YOUR<br/> WINTER <span className={s.hero__title_look}>LOOK</span>{" "}
+        <br/>APPAREL
       </h1>
       <CreateImage />
     </section>
